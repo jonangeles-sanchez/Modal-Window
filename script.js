683b43f95eac728f139e6d7b9ba4ab7a32b11f02
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-12-22 13:46:24 -0500

    modal setup
    Basic HTML & CSS to format visual appearance of the windows. JavaScript will be needed for actual functionality.

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".open-modal");
+console.log(btnsOpenModal);
