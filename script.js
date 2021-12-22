d9aae6d2725f68bb33987c650d3cbc3c6baca7a9
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-12-22 14:27:30 -0500

    Implemented modal window functionality by opening a new window of information
    Functionality includes: Opening a window with information and blurring the the area outside that window. The 'X' button closes the window and returns the website back to normal. Simply added a function to traverse through the modal buttons and called a function to listen for click events to add or remove hidden from the class.

--- script.js
+++ script.js
@@ -3,5 +3,18 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelector(".open-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+  btnCloseModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+}
