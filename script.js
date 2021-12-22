5a69c9215d4ff1d8c588b46f997ccf890e3cd0e6
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-12-22 15:02:39 -0500

    Reformat
    Reformatted code to use functions instead of repetitive code. Two functions were added. One to remove the hidden tag of a class. Another to add the hidden tag.

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-  btnCloseModal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
-}
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
